53991b59f2fc157a7f3b8167b256ae191e437bfe
Nimox Design <nimoxdesign@users.noreply.github.com>
2017-01-10 19:22:37 +0100

    Updated app.js, main logic should not be inside conditional statements

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,61 +21,63 @@
         calculatorError = document.getElementById('calculator-error');
 
     // check if there are any buttons
-    if (0 < calculatorFunctionalBtns.length) {
-        // loop over each button, assigning a listener for click events
-        calculatorFunctionalBtns.forEach(function (elem) {
-            elem.addEventListener('click', function (ev) {
-                // get the data-value
-                var btnValue = ev.target.getAttribute('data-value');
-
-                // concatenate the data-value with the input's value
-                calculatorInput.value += btnValue;
-            });
-        });
-
-        // listen if calculator should calculate
-        calculatorProcessor.addEventListener('click', function (ev) {
-            // set the input, just so it does not have to be passed to the calculator more than once
-            calculator.setInput(calculatorInput.value);
-            var result = calculator.calculate();
-
-            // if the calculation failed, undefined is returned
-            if (typeof result === 'undefined') {
-                calculatorErrors.style.display = 'block';
-                calculatorError.innerHTML = 'Error calculating: ' + calculator.getInput();
-                calculatorInput.value = 'ERROR';
-
-                return;
-            }
-
-            // calculation succeeded, make sure errors field is not displayed
-            calculatorErrors.style.display = '';
-
-            // replace the input's value with the new calculation
-            calculatorInput.value = result;
-        });
-
-        // listen for clearing the calculator input
-        calculatorClear.addEventListener('click', function (ev) {
-            calculatorInput.value = '';
-        });
-        
-        calculatorMinimize.addEventListener('click', function (ev) {
-            calculatorModule.classList.remove('is-closed');
-            calculatorModule.classList.remove('is-maximized');
-            calculatorModule.classList.toggle('is-minimized');
-        });
-        
-        calculatorMaximize.addEventListener('click', function (ev) {
-            calculatorModule.classList.remove('is-closed');
-            calculatorModule.classList.remove('is-minimized');
-            calculatorModule.classList.toggle('is-maximized');
-        });
-        
-        calculatorClose.addEventListener('click', function () {
-            calculatorModule.classList.remove('is-minimized');
-            calculatorModule.classList.remove('is-maximized');
-            calculatorModule.classList.toggle('is-closed');
-        });
+    if (!~calculatorFunctionalBtns.length) {
+        return;
     }
+    
+    // loop over each button, assigning a listener for click events
+    calculatorFunctionalBtns.forEach(function (elem) {
+        elem.addEventListener('click', function (ev) {
+            // get the data-value
+            var btnValue = ev.target.getAttribute('data-value');
+
+            // concatenate the data-value with the input's value
+            calculatorInput.value += btnValue;
+        });
+    });
+
+    // listen if calculator should calculate
+    calculatorProcessor.addEventListener('click', function (ev) {
+        // set the input, just so it does not have to be passed to the calculator more than once
+        calculator.setInput(calculatorInput.value);
+        var result = calculator.calculate();
+
+        // if the calculation failed, undefined is returned
+        if (typeof result === 'undefined') {
+            calculatorErrors.style.display = 'block';
+            calculatorError.innerHTML = 'Error calculating: ' + calculator.getInput();
+            calculatorInput.value = 'ERROR';
+
+            return;
+        }
+
+        // calculation succeeded, make sure errors field is not displayed
+        calculatorErrors.style.display = '';
+
+        // replace the input's value with the new calculation
+        calculatorInput.value = result;
+    });
+
+    // listen for clearing the calculator input
+    calculatorClear.addEventListener('click', function (ev) {
+        calculatorInput.value = '';
+    });
+        
+    calculatorMinimize.addEventListener('click', function (ev) {
+        calculatorModule.classList.remove('is-closed');
+        calculatorModule.classList.remove('is-maximized');
+        calculatorModule.classList.toggle('is-minimized');
+    });
+        
+    calculatorMaximize.addEventListener('click', function (ev) {
+        calculatorModule.classList.remove('is-closed');
+        calculatorModule.classList.remove('is-minimized');
+        calculatorModule.classList.toggle('is-maximized');
+    });
+        
+    calculatorClose.addEventListener('click', function () {
+        calculatorModule.classList.remove('is-minimized');
+        calculatorModule.classList.remove('is-maximized');
+        calculatorModule.classList.toggle('is-closed');
+    });
 }());
